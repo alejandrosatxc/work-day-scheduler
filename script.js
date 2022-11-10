bdff66031f093ca13fbd748a72661e6c326a68d4
AlejandroZapien <alejandrozapien09@gmail.com>
2022-11-09 19:24:45 -0600

    Update documentation, change color of textarea

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-var scheduledEvents = []
+//Global variables that are accessible in every function
+var scheduledEvents = [] //array to hold saved and loaded scheduled events
 var workDayHours = 8 //one timeblock for each hour in a workday
-var workDayStartHour = 4 //pick and hour between 0-23
+var workDayStartHour = 14 //pick and hour between 0-23
 
+//First, start a clock, then create timeblocks, then add event listeners to buttons
 startClock()
-initTimeBlocks()
+createTimeBlocks()
 //TODO: bug click donesnt work when clicking directly on save icon
 $('.save-btn').on('click', saveScheduledEvent)
 
-
+//This function starts a clock and displays it in the header of the page
+//Note: since the requirements only require day of the week, month, and day
+//It is unecessary to use setInterval every second, since we are not 
+//displaying seconds
 function startClock() {
     $('#currentDay').text(dayjs().format("dddd, MMMM D"))
     var ClockInterval = setInterval(function () {
@@ -15,25 +20,36 @@ function startClock() {
     }, 1000)
 }
 
-function initTimeBlocks() {
-    //For a x hour workday, create x timeblocks
+//This function dynamically creates timeblocks based on the number of workDayHours and
+//The hour, workDayStartHour (0-23), at which the work day starts.
+function createTimeBlocks() {
+    
+    //These variables will be used hold newly created elements that make up a timeblock
     var newTimeblock = null
     var hour = null
     var textarea = null
     var saveButton = null
-    //For each hour in the workday, insert a new timeblock
+    var currentHour = dayjs().hour() //use day.js to get the current hour as an integer(0-23)
+    //For each hour in the workday, create a new timeblock
     for (var i = 0; i < workDayHours; i++) {
-        //Create a timeblock using jquery
+        //Create a div that represents a timeblock using jquery
         newTimeblock = $('<div>').addClass('row justify-content-between time-block')
+        //We could also use id's here, however I felt better about using data attributes instead
+        //of a bunch of different id's that look like id='1', id='2', id='3' etc...
         newTimeblock.attr('data-hour', i)
+        //Create div that holds the hour
+        //Use a mix of jQuery and Day.js to dynamically update the hour in each time block with proper formatting
         hour = $('<div>').text(dayjs().hour(workDayStartHour).add(i, 'hour').format("h A")).addClass('hour')
+        //Create <textarea> for user input
         textarea = $('<textarea>').addClass('flex-grow-1')
         //Apply a class to change textarea color based on the current time
-        if(dayjs().hour()  >  (workDayStartHour + i)) { textarea.addClass('past')    }
-        if(dayjs().hour() === (workDayStartHour + i)) { textarea.addClass('present') }
-        if(dayjs().hour()  <  (workDayStartHour + i)) { textarea.addClass('future')  }
+        if(currentHour  >  (workDayStartHour + i)) { textarea.addClass('past')    }
+        if(currentHour === (workDayStartHour + i)) { textarea.addClass('present') }
+        if(currentHour  <  (workDayStartHour + i)) { textarea.addClass('future')  }
+        //Create a button to save user input to localStorage
         saveButton = $('<button>').addClass('save-btn')
         saveButton.append('<i class="fas fa-save"></i>') //add fontawesome icon
+        //Append all of the newly created elements inside a timeblock element
         newTimeblock.append(hour).append(textarea).append(saveButton)
         //Append timeblock to main container
         $('.container').append(newTimeblock)
@@ -42,8 +58,11 @@ function initTimeBlocks() {
     loadScheduledEvents()
 }
 
+//This function creates an object (scheduledEvent) to store user input, 
+//then inserts that object into the global array of scheduled events
+//then saves that array into localStorage using the name 'scheduledEvents'
 function saveScheduledEvent(e) {
-    console.log('You clicked: ' + $(e.target).siblings('.hour').text() + " with value: " + $(e.target).siblings('textarea').val())
+    //Create an event to store user input 'text' 
     var scheduledEvent = {
         time: $(e.target).siblings('.hour').text(),
         text: $(e.target).siblings('textarea').val()
@@ -56,6 +75,9 @@ function saveScheduledEvent(e) {
     localStorage.setItem("scheduledEvents", JSON.stringify(scheduledEvents))
 }
 
+//This function checks localStorage for 'scheduledEvents'
+//If it finds some, it will load in the data into each timeblock
+//Otherwise, it fills the global array of scheduled events with placeholder objects
 function loadScheduledEvents() {
     //Read from localStorage
     scheduledEvents = localStorage.getItem('scheduledEvents')
@@ -77,8 +99,8 @@ function loadScheduledEvents() {
         scheduledEvents = JSON.parse(scheduledEvents)
         //Target all textareas in timeblocks
         var timeBlocks = $('textarea')
+        //for each textarea, change the text in it to what was saved in localStorage
         for (var i in scheduledEvents) {
-            console.log(scheduledEvents[i])
             $(timeBlocks[i]).val(scheduledEvents[i].text)
         }
     }
